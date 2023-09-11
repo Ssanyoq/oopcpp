f046fc545a3f79d62176b4e0294ecb2c7d6c68ad
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-09-11 22:21:51 +0300

    Everything is now in MatrixSpace namespace

--- lab1/matrix.h
+++ lab1/matrix.h
@@ -14,64 +14,65 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+namespace MatrixSpace {
 /**
  * Universal Matrix class
  * @tparam T - any type that can be printed out
  */
-template<typename T>
-class Matrix {
-private:
+    template<typename T>
+    class Matrix {
+    private:
 
-    std::map<std::size_t, std::map<std::size_t, T> > lines;
-public:
-    const std::size_t height;
-    const std::size_t width;
+        std::map<std::size_t, std::map<std::size_t, T> > lines;
+    public:
+        const std::size_t height;
+        const std::size_t width;
 
-    Matrix(std::size_t colsNum, std::size_t rowsNum) : width(colsNum), height(rowsNum),
-                                                       lines(std::map<std::size_t, std::map<std::size_t, T> >()) {
-    };
+        Matrix(std::size_t colsNum, std::size_t rowsNum) : width(colsNum), height(rowsNum),
+                                                           lines(std::map<std::size_t, std::map<std::size_t, T> >()) {
+        };
 
-    /**
-     * Value setter for the matrix
-     * @param i row index
-     * @param j column index
-     * @param value value to be set
-     * @throws std::invalid_argument if i or j are out of bounds
-     */
-    void setValue(std::size_t i, std::size_t j, const T value) {
-        if (i < height && j < width) {
-            if (value == NULL) {
-                if (getValue(i, j) != NULL) {
-                    lines[i][j] = value;
-                    return;
+        /**
+         * Value setter for the matrix
+         * @param i row index
+         * @param j column index
+         * @param value value to be set
+         * @throws std::invalid_argument if i or j are out of bounds
+         */
+        void setValue(std::size_t i, std::size_t j, const T value) {
+            if (i < height && j < width) {
+                if (value == NULL) {
+                    if (getValue(i, j) != NULL) {
+                        lines[i][j] = value;
+                        return;
+                    }
                 }
-            }
-            if (lines.find(i) == std::end(lines)) {
-                lines[i] = std::map<std::size_t, T>();
-            }
-            lines[i][j] = value;
-        } else {
-            throw std::range_error("Indices are out of bounds");
-        }
-    }
-
-    /**
-     * Value getter for the matrix, gets [i][j] element
-     * @param i row index
-     * @param j column index
-     * @throws std::invalid_argument if i or j are out of bounds
-     */
-    T getValue(std::size_t i, std::size_t j) {
-        if (i < height && j < width) {
-            if (lines[i].find(j) != std::end(lines[i])) {
-                return lines[i].at(j);
+                if (lines.find(i) == std::end(lines)) {
+                    lines[i] = std::map<std::size_t, T>();
+                }
+                lines[i][j] = value;
             } else {
-                return 0;
+                throw std::range_error("Indices are out of bounds");
             }
-        } else {
-            throw std::range_error("Indices are out of bounds");
         }
-    }
+
+        /**
+         * Value getter for the matrix, gets [i][j] element
+         * @param i row index
+         * @param j column index
+         * @throws std::invalid_argument if i or j are out of bounds
+         */
+        T getValue(std::size_t i, std::size_t j) {
+            if (i < height && j < width) {
+                if (lines[i].find(j) != std::end(lines[i])) {
+                    return lines[i].at(j);
+                } else {
+                    return 0;
+                }
+            } else {
+                throw std::range_error("Indices are out of bounds");
+            }
+        }
 
 //    /**
 //     * Overloading << operator for correct printing of the Matrix
@@ -89,20 +90,20 @@ public:
 //        }
 //        return os;
 //    }
-};
+    };
 
-template<typename T>
-void printMatrix(Matrix<T> &matrix) {
-    cout << "Matrix " << matrix.height << "x" << matrix.width << endl;
-    for (int i = 0; i < matrix.height; i++) {
-        for (int j = 0; j < matrix.width; j++) {
-            cout << matrix.getValue(i, j);
-            if (j != matrix.width - 1) {
-                cout << " ";
+    template<typename T>
+    void printMatrix(Matrix<T> &matrix) {
+        cout << "Matrix " << matrix.height << "x" << matrix.width << endl;
+        for (int i = 0; i < matrix.height; i++) {
+            for (int j = 0; j < matrix.width; j++) {
+                cout << matrix.getValue(i, j);
+                if (j != matrix.width - 1) {
+                    cout << " ";
+                }
             }
+            cout << endl;
         }
-        cout << endl;
     }
 }
-
 #endif
--- lab1/vector_maker.h
+++ lab1/vector_maker.h
@@ -4,22 +4,39 @@
 #include <vector>
 #include <iostream>
 
-template<typename T>
-std::vector<T> makeVector(Matrix<T> matrix, bool(*f)(T)) {
-    std::vector<T> vector(matrix.height);
-    for (int i = 0; i < matrix.height; i++) {
-        for (int j = 0; j < matrix.width; j++) {
-            T val = matrix.getValue(i, j);
-            if (f(val)) {
-                vector[i] += val;
+
+
+namespace MatrixSpace {
+
+    template<typename T>
+    void printVector(std::vector<T> vector) {
+        cout << "Vector " << vector.size() << "x1:" << endl;
+        for (int i = 0; i < vector.size(); i++) {
+            cout << vector[i];
+            if (i != vector.size() - 1) {
+                cout << " ";
             }
         }
+        cout << endl;
     }
-    return vector;
-}
 
-template <typename T>
-bool criteria(T element) {
-    return element > 10;
+    template<typename T>
+    std::vector<T> makeVector(Matrix<T> matrix, bool(*f)(T)) {
+        std::vector<T> vector(matrix.height);
+        for (int i = 0; i < matrix.height; i++) {
+            for (int j = 0; j < matrix.width; j++) {
+                T val = matrix.getValue(i, j);
+                if (f(val)) {
+                    vector[i] += val;
+                }
+            }
+        }
+        return vector;
+    }
+
+    template<typename T>
+    bool criteria(T element) {
+        return element > 10;
+    }
 }
 #endif //LAB1_VECTOR_MAKER_H
