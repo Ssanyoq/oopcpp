607d47200f867b3ff975c380a5f58a73e214d373
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-09-27 09:38:00 +0300

    Better .h/.cpp division for the basic class

--- lab2/octet.cpp
+++ lab2/octet.cpp
@@ -1,75 +1,64 @@
 #include "octet.h"
 
 
-class Octet {
-private:
-    uint8_t data;
-public:
-    explicit Octet(const int byte[8]) {
-        data = 0;
-        for (int i = 0; i < 8; i++) {
-            data = data << 1;
-            data += byte[i];
-        }
+Octet::Octet(const int byte[8]) {
+    data = 0;
+    for (int i = 0; i < 8; i++) {
+        data = data << 1;
+        data += byte[i];
     }
-
-    explicit Octet(uint8_t byte) : data(byte) {
-    };
+}
 
 
-    Octet operator+(Octet other) const {
-        return Octet(other.data + data);
+Octet Octet::operator+(Octet other) const {
+    return Octet(other.data + data);
+}
+
+Octet Octet::operator&(Octet other) const {
+    return Octet(other.data & data);
+}
+
+bool Octet::operator[](int index) const {
+    if (index < 0 or index > 7) {
+        throw std::out_of_range("Index is out of range");
     }
-
-    Octet operator&(Octet other) const {
-        return Octet(other.data & data);
+    if ((1 << (7 - index)) & data) {
+        return true;
+    } else {
+        return false;
     }
+}
 
-    bool operator[](int index) const {
-        if (index < 0 or index > 7) {
-            throw std::out_of_range("Index is out of range");
-        }
-        if ((1 << (7 - index)) & data) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    Octet operator~() const {
-        return Octet(~data);
-    };
-
-    Octet add(Octet other, bool *flag = nullptr) const {
-        uint8_t sum = 0;
-        bool buffer = false;
-        uint8_t curWeight = 1;
-        for (int i = 7; i > -1; i--) {
-            bool otherBit = other[i];
-            bool thisBit = this->operator[](i);
-
-            sum += (otherBit ^ thisBit ^ buffer) * curWeight;
-
-            buffer = (thisBit & otherBit) | (buffer & (thisBit ^ otherBit));
-
-            curWeight *= 2;
-        }
-        if (flag != nullptr) {
-            *flag = buffer;
-        }
-        return Octet(sum);
-    }
-
-    int getAddition() const {
-        return 256 - data;
-    }
-
-    friend std::ostream &operator<< (std::ostream &os, Octet octet);
+Octet Octet::operator~() const {
+    return Octet(~data);
 };
 
+Octet Octet::add(Octet other, bool *flag) const {
+    uint8_t sum = 0;
+    bool buffer = false;
+    uint8_t curWeight = 1;
+    for (int i = 7; i > -1; i--) {
+        bool otherBit = other[i];
+        bool thisBit = this->operator[](i);
+
+        sum += (otherBit ^ thisBit ^ buffer) * curWeight;
+
+        buffer = (thisBit & otherBit) | (buffer & (thisBit ^ otherBit));
+
+        curWeight *= 2;
+    }
+    if (flag != nullptr) {
+        *flag = buffer;
+    }
+    return Octet(sum);
+}
 
 
-std::ostream &operator<< (std::ostream &os, Octet octet) {
+int Octet::getAddition() const {
+    return 256 - data;
+}
+
+std::ostream &operator<<(std::ostream &os, Octet octet) {
     os << "Octet: " << endl;
     for (int i = 0; i < 8; i++) {
         os << octet[i] << " ";
@@ -80,7 +69,7 @@ std::ostream &operator<< (std::ostream &os, Octet octet) {
 
 int main() {
     int byte[] = {0, 0, 0, 0, 0, 0, 1, 0};
-    int byte2[] = {1, 0,0,1,0,0,0,0};
+    int byte2[] = {1, 0, 0, 1, 0, 0, 0, 0};
     Octet oct(byte);
     Octet oct2(byte2);
     cout << oct << oct2 << endl;
--- lab2/octet.h
+++ lab2/octet.h
@@ -15,8 +15,31 @@ using std::endl;
 using std::string;
 
 class Octet {
+private:
+    uint8_t data;
+public:
+    explicit Octet(const int byte[8]);
 
+    explicit Octet(uint8_t byte) : data(byte){};
+
+    Octet operator+(Octet other) const;
+
+    Octet operator&(Octet other) const;
+
+    bool operator[](int index) const;
+
+    Octet operator~() const;
+
+    Octet add(Octet other, bool *flag = nullptr) const;
+
+    int getAddition() const;
+
+    friend std::ostream &operator<< (std::ostream &os, Octet octet);
 };
+
+
+
+
 
 
 
