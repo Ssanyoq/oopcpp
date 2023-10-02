b678dc5218a4f2ad0ec0301eacaaf2aebce1793d
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-10-03 01:19:58 +0300

    No vectors, working [], getTwosCompliment() and getLength()

--- lab2/binary.cpp
+++ lab2/binary.cpp
@@ -1,15 +1,16 @@
-#include "octet.h"
 #include "binary.h"
 
 /**
- * Двоичное число в ПК
+ * Двоичное число в Двоичном коде, знак кодируется 1 битом
  *
  * @see Octet в octet.cpp
  */
 class BinaryNumber {
 private:
-    vector<Octet> octets; // data storage
     int numberStart; // starting position
+
+    size_t octetsLength;
+    Octet *octets; // data storage
 public:
 
     /**
@@ -19,10 +20,8 @@ public:
      * (первый бит - знак числа)
      */
     explicit BinaryNumber(const string &binaryNumber) {
-        if (binaryNumber == "") {
-            octets = vector<Octet>(1);
-            numberStart = 7;
-            return;
+        if (binaryNumber.empty()) {
+            BinaryNumber(string("0"));
         }
         size_t len = binaryNumber.length() / 8;
         if (binaryNumber.length() % 8 != 0) {
@@ -31,7 +30,8 @@ public:
         } else {
             numberStart = 0;
         }
-        octets = vector<Octet>(len);
+        octets = new Octet[len];
+        octetsLength = len;
 
         for (int i = 0; i < binaryNumber.length(); i++) {
             if (binaryNumber[i] == '1') {
@@ -42,18 +42,69 @@ public:
         }
     }
 
-//    explicit BinaryNumber(long number) {
-//    }
+    explicit BinaryNumber(long number) {
+        BinaryNumber(longToBinary(number));
+    }
 
-//    BinaryNumber getTwosComplement() const {
-//    }
+    size_t getLength() const {
+        return octetsLength * 8 - numberStart;
+    }
+
+    /**
+     * Возвращает бит числа под индексом index. Начинается с 0 и нулевой бит - знак
+     * @param index int
+     * @throws std::out_of_range если индекс выходит за рамки разрешенного
+     */
+    bool operator [](int index) const {
+        if (index < 0 or index >= getLength()) {
+            throw std::out_of_range("Index is out of range");
+        }
+        size_t octet_i = (index + numberStart) / 8;
+        size_t bit_i = (index + numberStart) % 8;
+        return octets[octet_i][bit_i];
+    }
+
+
+    /**
+     * Возвращает экземпляр класса в дополнительном коде(если код этого числа - прямой)
+     */
+    BinaryNumber getTwosComplement() const {
+        string newNum;
+        bool sign = operator[](0);
+        newNum.push_back(binaryToChar(sign));
+        for (int i = 1; i < getLength(); i++) {
+            bool newBit = operator[](i);
+            if (sign) {
+                newBit = !newBit;
+            }
+            newNum.push_back(binaryToChar(newBit));
+        }
+
+        if (!sign) {
+            return BinaryNumber(newNum);
+        }
+        bool buf = true;
+        for (int i = getLength() - 1; i >= 0; i--) {
+            if (buf) {
+                if (newNum[i] == '1') {
+                    newNum[i] = '0';
+                } else {
+                    newNum[i] = '1';
+                    buf = false;
+                    break;
+                }
+            }
+        }
+        return BinaryNumber(newNum);
+    }
+
     friend std::ostream &operator<<(std::ostream &os, BinaryNumber binaryNumber);
 
 };
 
 std::ostream &operator<<(std::ostream &os, BinaryNumber binaryNumber) {
     os << "Number: " << "start at index " << binaryNumber.numberStart << endl;
-    for (int i = 0; i < binaryNumber.octets.size(); i++) {
+    for (int i = 0; i < binaryNumber.octetsLength; i++) {
         os << binaryNumber.octets[i];
     }
     os << endl;
@@ -62,7 +113,9 @@ std::ostream &operator<<(std::ostream &os, BinaryNumber binaryNumber) {
 
 int main() {
 
-    BinaryNumber b("1001");
+    BinaryNumber b("1001000");
 
     cout << b;
+    cout << b.getLength() << endl << b[0] << endl;
+    cout << b.getTwosComplement();
 }
