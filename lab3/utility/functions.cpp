0fd4a863b1ff726ed779b2b9cc6a743f51da9557
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-11-28 15:17:04 +0300

    Working Lee's path finding algorithm

--- lab3/utility/functions.cpp
+++ lab3/utility/functions.cpp
@@ -1,5 +1,104 @@
-//
-// Created by Aleksandr Kutsalo on 21.11.2023.
-//
-
 #include "functions.h"
+#include <iostream>
+
+
+inline double getDistance(const Coordinates &first, const Coordinates &second) {
+    return sqrt(pow(first.x - second.x, 2) +
+                pow(first.y - second.y, 2));
+}
+
+double getDistance(const Sprite &first, const Sprite &second) {
+    return getDistance(first.getPos(), second.getPos());
+}
+
+double calculateAngle(const Coordinates &from, const Coordinates &to) {
+    return atan2(to.y - from.y, to.x - from.x);
+}
+
+vector<Coordinates> getPath(const vector<vector<int>> &distances, Coordinates to) {
+    int deltaY[] = {-1, 0, 1, 0};
+    int deltaX[] = {0, 1, 0, -1};
+    vector<Coordinates> reversedPath; // reversed!
+    auto curNum = distances[to.y][to.x];
+    auto curCoords = to;
+    reversedPath.push_back(to);
+
+    while (curNum != 0) {
+        int minDist = -1;
+        Coordinates minCoords;
+        for (int i = 0; i < 4; i++) {
+            auto newCoords = curCoords;
+            newCoords.x += deltaX[i];
+            newCoords.y += deltaY[i];
+            if (newCoords.y >= distances.size() || newCoords.y < 0 ||
+                newCoords.x >= distances[0].size() || newCoords.x < 0 || distances[newCoords.y][newCoords.x] == -1
+                    ) {
+                continue;
+            }
+            auto newNum = distances[newCoords.y][newCoords.x];
+            if (minDist == -1 || minDist > newNum) {
+                minDist = newNum;
+                minCoords = newCoords;
+            }
+        }
+        curNum = minDist;
+        curCoords = minCoords;
+        reversedPath.push_back(curCoords);
+    }
+    vector<Coordinates> path;
+    for (int i = reversedPath.size() - 1; i >= 0; i--) {
+        path.push_back(reversedPath[i]);
+    }
+    return path;
+}
+
+vector<vector<int>> getDistances(vector<vector<TileType>> map, const Coordinates &from) {
+    int deltaY[] = {-1, 0, 1, 0};
+    int deltaX[] = {0, 1, 0, -1};
+    auto height = map.size(), width = map[0].size();
+    vector<vector<int>> distances(height, vector<int>(width, 0));
+    // x - width, y - height, so distances[y][x]
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (map[i][j] != Road) {
+                distances[i][j] = -1;
+            }
+            std::cout << distances[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    queue<Coordinates> traversalQueue;
+    traversalQueue.push(from);
+
+    while (!traversalQueue.empty()) {
+        auto current = traversalQueue.front();
+        int minNeighbor = -1;
+        for (int i = 0; i < 4; i++) {
+            // generating new coordinates
+            auto newCoords = current;
+            newCoords.x += deltaX[i];
+            newCoords.y += deltaY[i];
+
+            // Checking if there is obstacle or if they are out of bounds
+            if (newCoords.x >= width || newCoords.x < 0 || newCoords.y >= height || newCoords.y < 0 ||
+                distances[newCoords.y][newCoords.x] == -1) {
+                continue;
+            }
+
+            // Making new number for this cell if neighbor was checked already
+            if (distances[newCoords.y][newCoords.x] != 0 || (newCoords.x == from.x && newCoords.y == from.y)) {
+                if (minNeighbor == -1 || minNeighbor > distances[newCoords.y][newCoords.x]) {
+                    minNeighbor = distances[newCoords.y][newCoords.x];
+                }
+            } else {
+                traversalQueue.push(newCoords);
+            }
+        }
+
+        distances[current.y][current.x] = minNeighbor + 1;
+        traversalQueue.pop();
+    }
+
+    return distances;
+}
--- lab3/utility/functions.h
+++ lab3/utility/functions.h
@@ -6,31 +6,26 @@
 #define LAB3_FUNCTIONS_H
 
 #include "../objects/sprite.h"
+#include "../objects/tile_type.h"
+
 #include "coords.h"
 #include <cmath>
+#include <vector>
+#include <queue>
 
-double getSpriteDistance(const Sprite &first, const Sprite &second) {
-    return sqrt(pow(first.getPos().x - second.getPos().x, 2) +
-                pow(first.getPos().y - second.getPos().y, 2));
-}
-double getDistance(const Coordinates &first, const Coordinates &second) {
-    return sqrt(pow(first.x - second.x, 2) +
-                pow(first.y - second.y, 2));
-}
-
-double calculateAngle(const Coordinates &from, const Coordinates &to) {
-    return atan2(to.y - from.y, to.x - from.x);
-}
+using std::queue;
+using std::vector;
 
 
-vector<Coordinates> getShortestPath(const Map &map, const Coordinates &first, const Coordinates &second) {
-    vector<vector<bool>> visited;
-    for (int i = 0; i < map.getHeight(); i++) {
-        vector<bool> vect(map.getWidth(), false);
-        visited.push_back(vect);
-    }
-    return vector<Coordinates>(1, Coordinates{.x = 0, .y = 0}); // TODO
+double getDistance(const Coordinates &first, const Coordinates &second);
 
-}
+double getDistance(const Sprite &first, const Sprite &second);
+
+double calculateAngle(const Coordinates &from, const Coordinates &to);
+
+
+vector<vector<int>> getDistances(vector<vector<TileType>> map, const Coordinates &from);
+
+vector<Coordinates> getPath(const vector<vector<int>>& distances, int x, int y);
 
 #endif //LAB3_FUNCTIONS_H
