124af25e55ccb33128899a427b3d34c222b4e85a
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-12-05 22:57:33 +0300

    Main game class, coins features are not implemented yet

--- lab3/game_features/game.cpp
+++ lab3/game_features/game.cpp
@@ -1,14 +1,17 @@
 #include "game.h"
 
-Game::Game() {
 
-}
+Game::Game() = default;
 
-void Game::addEntity(const Entity& newEntity) {
+void Game::addEntity(Entity& newEntity) {
     entities.push_back(newEntity);
 }
-void Game::addPlaceable(const Placeable &newPlaceable) {
-    placeables.push_back(newPlaceable);
+void Game::addDefence(Defence &newDefence) {
+    auto tile = currentMap.getTile(newDefence.getPos()); // TODO pointer
+    if (tile->getContents() == nullptr) {
+        tile->setContents((Placeable *) &newDefence);
+    }
+    placeables.push_back(newDefence);
 }
 
 void Game::deleteEntity(int index) {
@@ -22,16 +25,17 @@ void Game::deletePlaceable(int index) {
     if (index < 0 or index > placeables.size() - 1) {
         throw std::out_of_range("Object with this index does not exist");
     }
+    currentMap.getTile(placeables[index].getPos())->setContents(nullptr);
     placeables.erase(placeables.begin() + index);
 }
 
 
 void Game::moveEntities() {
     for (int i = 0; i < entities.size(); i++) {
-        entities[i].move();
         if (!entities[i].isAlive()) {
             deleteEntity(i);
         }
+        entities[i].move(currentMap.getCastle());
     }
 }
 void Game::usePlaceables() {
@@ -46,13 +50,9 @@ void Game::process() {
 
 }
 
-bool Game::place(Placeable *newObject, int x, int y) {
-    Tile tile = currentMap.getTile(x, y);
-    if (tile.getContents() == nullptr) {
-        tile.setContents(newObject);
-        return true;
-    }
-    return false;
+bool Game::place(const Placeable& newObject, int x, int y) {
+    placeables.push_back(newObject);
+    return true; //TODO
 }
 
 
--- lab3/game_features/game.h
+++ lab3/game_features/game.h
@@ -3,25 +3,25 @@
 
 #include "map.h"
 #include <vector>
+#include "defence.h"
 #include "../entities/entity.h"
 #include "../objects/placeable.h"
 
 class Game {
 protected:
-    std::vector<Entity> entities;
-    std::vector<Placeable> placeables;
-    Map currentMap;
     void moveEntities();
     void usePlaceables();
-
-    void addEntity(const Entity& newEntity);
-    void addPlaceable(const Placeable &newPlaceable);
+    void addEntity( Entity& newEntity);
+    void addDefence( Defence &newDefence); // TODO
     void deleteEntity(int index);
     void deletePlaceable(int index);
 public:
+    std::vector<Entity> entities;
+    std::vector<Placeable> placeables;
+    Map currentMap = Map({{Road}}, {.x = 0, .y = 0}, {});
     Game();
     void process();
-    bool place(Placeable *newObject, int x, int y);
+    bool place(const Placeable &newObject, int x, int y);
 };
 
 #endif //OOPCPP_GAME_H
