ed230614f82fb8c6d6b20960b6da9fd34ca57ce9
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-12-14 13:02:59 +0300

    doAction() refactoring

--- lab3/objects/placeable.h
+++ lab3/objects/placeable.h
@@ -3,12 +3,22 @@
 
 #include "sprite.h"
 
+enum ObjectType { // TODO
+    Base,
+    Turret,
+    Spawn,
+    Other,
+};
+class Entity;
+
 class Placeable : public Sprite {
 public:
     Placeable(long long int x=-1, long long int y=-1, short rotation=0) : Sprite(x, y,
                                                                          rotation) {};
 
-    virtual void doAction() {};
+    virtual void doAction(std::vector<Entity> &entities) {};
+
+    virtual ObjectType getType() const {return Other;}; // TODO
 };
 
 #endif
